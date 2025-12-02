f8ec6ce8e97dc336ff04f3d0b18972e6f9458b10
MaxHass12 <shwetank.tewari.87@gmail.com>
2025-12-02 13:23:37 -0500

    changed session_store settings

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,11 @@
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store,
+    key: "_swftlabs_session",
+    same_site: :none,
+    secure: true
+else
+  Rails.application.config.session_store :cookie_store,
+    key: "_swftlabs_session",
+    same_site: :lax,
+    secure: false
+end
