467a703115e2acd1be7fbb6f4717fc0bcf2a5a62
MaxHass12 <shwetank.tewari.87@gmail.com>
2025-12-02 13:33:59 -0500

    disabled authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,11 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id]) 
+    else
+      @current_user = User.find_by(email: "admin@swftlabs.com")
+    end
   end
 
   def logged_in?
