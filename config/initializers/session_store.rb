67fbaff79549d5f54b7703f973423824bf3285ea
MaxHass12 <shwetank.tewari.87@gmail.com>
2025-12-09 13:06:34 -0500

    fixed typo on session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     expires_after: 24.hours,
     same_site: :none,
     secure: true
-else if Rails.env.staging?
+elsif Rails.env.staging?
   Rails.application.config.session_store :cookie_store,
     key: "_swftlabslims_session",
     domain: ".swftserver.com", 
@@ -15,7 +15,6 @@ else if Rails.env.staging?
 else
   Rails.application.config.session_store :cookie_store,
     key: "_swftlabslims_session",
-    domain: ".localhost", 
     expires_after: 24.hours,
     same_site: :lax,
     secure: false
